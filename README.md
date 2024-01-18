d79833122514d76e60f6b7a8d7967f4666cdb1cf
jonybilstein <37225105+jonybilstein@users.noreply.github.com>
2024-01-18 13:22:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MortgageCalcMVP
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jonybilstein/MortgageCalcMVP)
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import '@angular/localize/init';
+import {importProvidersFrom} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {VERSION as CDK_VERSION} from '@angular/cdk';
+import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
+import {GridListOverviewExample} from './example/grid-list-overview-example';
+
+
+
+/* eslint-disable no-console */
+console.info('Angular CDK version', CDK_VERSION.full);
+console.info('Angular Material version', MAT_VERSION.full);
+
+bootstrapApplication(GridListOverviewExample, {
+  providers: [
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(MatNativeDateModule)
+  ]
+}).catch(err => console.error(err));
